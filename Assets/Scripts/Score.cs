16638d1fdeac55d5b2590915fe2a906f5a5007b1
Filip Lundby <filip@filiplundby.dk>
2022-05-01 11:20:53 +0200

    Combo score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -9,14 +9,51 @@ public class Score : MonoBehaviour
 
     private int scoreValue = 0;
 
+    private List<CardData> combos = new List<CardData>();
+
     public void AddScore(string cardName, int value) 
     {
-        scoreValue += value;
+        // Compare current card to the one added latest
+        if (combos.Count > 0) 
+        {
+            CardData lastCombo = combos[combos.Count - 1];
+            if (lastCombo.CardName == cardName) 
+            {
+                // Change this calculation to whatever
+                scoreValue += lastCombo.CardValue * value; 
+            }
+            else 
+            {
+                // Remove all combos
+                combos.Clear();
+            }
+        }
+
+        // If no combos - just add up 
+        if (combos.Count <= 0) 
+        {
+            scoreValue += value;
+        }
+
+        // Update text
         GetComponent<TMPro.TextMeshProUGUI>().text = $"SCORE {scoreValue.ToString()}";
+
+        // Remember combos 
+        combos.Add(new CardData() { 
+            CardName = cardName,
+            CardValue = value
+        });
     }
 
     void Awake()
     {
         instance = this;
     }
+}
+
+
+public class CardData
+{
+    public string CardName;
+    public int CardValue;
 }
