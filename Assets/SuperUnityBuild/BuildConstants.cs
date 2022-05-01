de8062bbf1de8677fae6cd732718ecd9d6825745
Pat Scott <git@patrick-scott.com>
2022-05-01 14:41:25 +0200

    build ish

--- Assets/SuperUnityBuild/BuildConstants.cs
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -12,12 +12,14 @@ public static class BuildConstants
     {
         None,
         WebGL,
+        PC,
     }
 
     public enum Architecture
     {
         None,
         WebGL,
+        Windows_x86,
     }
 
     public enum Distribution
@@ -26,11 +28,11 @@ public static class BuildConstants
         Itch,
     }
 
-    public static readonly System.DateTime buildDate = new System.DateTime(637869981014431854);
-    public const string version = "1.0.0.1";
+    public static readonly System.DateTime buildDate = new System.DateTime(637870124564919315);
+    public const string version = "1.0.0.4";
     public const ReleaseType releaseType = ReleaseType.Release;
-    public const Platform platform = Platform.WebGL;
-    public const Architecture architecture = Architecture.WebGL;
-    public const Distribution distribution = Distribution.Itch;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x86;
+    public const Distribution distribution = Distribution.None;
 }
 
